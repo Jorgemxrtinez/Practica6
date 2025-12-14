b44895d6e5a56f160752c4b0b4233f70f65cf2b9
Andres Hernandez <ignaciospagnol@gmail.com>
2025-12-14 17:10:45 +0100

    Estructura inicial del proyecto

---
+++ src/test/java/P6_Cicatrack/P6/P6ApplicationTests.java
@@ -0,0 +1,13 @@
+package P6_Cicatrack.P6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
